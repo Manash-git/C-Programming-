26562ab58c08d637cfbf5eeebb1690e2d16d6830
Manash-git <manashmondalcse@gmail.com>
2017-10-24 01:54:45 +0600

    initial commit

---
+++ file/fgets().cpp
@@ -0,0 +1,40 @@
+//#include<stdio.h>
+#include<conio.h>
+#include<bits/stdc++.h>
+
+int main(){
+
+	FILE *file;
+
+	file= fopen("file read().txt","r");		// file initialize append
+
+
+	if (file==NULL){
+		printf(" File doesn't exist.\n");
+	}else {
+		printf(" File exist and opened.\n");
+/*		char name[100];
+		int age=0;
+
+		printf(" Enter ur name:: ");
+		gets(name);
+
+		printf("\n Enter ur age :: ");
+		scanf("%d",&age);
+
+		fprintf(file," Name:: %s \t Age :: %d \n",name,age);
+
+        printf(" File is written successfully.\n");
+*/
+	char data[100];
+	while (!feof(file)) {
+        fgets(data,50,file);
+        printf("%s\n",data);
+
+	}
+		fclose(file);
+	}
+
+	getch();
+	return 0;
+}
